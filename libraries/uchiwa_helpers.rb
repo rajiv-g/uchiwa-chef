7c820bf02cc178933ebcad5573981dfb83ffcad9
Rajiv <rajiv.g@introhive.com>
2018-03-07 17:30:18 +0530

    Allow uchiwa to use chef-vault

--- libraries/uchiwa_helpers.rb
+++ libraries/uchiwa_helpers.rb
@@ -1,9 +1,25 @@
 module Uchiwa
   module Helpers
 
+    extend ChefVaultCookbook if Kernel.const_defined?("ChefVaultCookbook")
     def self.data_bag_item(data_bag_name, data_bag_item, missing_ok=false)
 
-      Chef::EncryptedDataBagItem.load(data_bag_name, data_bag_item).to_hash.delete_if { |key, value| key == 'id' }
+      raw_hash = Chef::DataBagItem.load(data_bag_name, data_bag_item)
+      encrypted = raw_hash.detect do |key, value|
+        if value.is_a?(Hash)
+          value.has_key?("encrypted_data")
+        end
+      end
+      if encrypted
+        if Chef::DataBag.load(data_bag_name).key? "#{data_bag_item}_keys"
+          chef_vault_item(data_bag_name, data_bag_item)
+        else
+          secret = Chef::EncryptedDataBagItem.load_secret
+          Chef::EncryptedDataBagItem.new(raw_hash, secret)
+        end
+      else
+        raw_hash
+      end
 
     rescue Chef::Exceptions::ValidationFailed,
         Chef::Exceptions::InvalidDataBagPath,
