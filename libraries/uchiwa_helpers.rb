a2555bc6c3511709ee2fd2c85236f71c76d9f5af
Rajiv <rajiv.g@introhive.com>
2018-03-08 13:26:54 +0530

    Using Chef-Vault helpers

--- libraries/uchiwa_helpers.rb
+++ libraries/uchiwa_helpers.rb
@@ -4,21 +4,15 @@ module Uchiwa
     extend ChefVaultCookbook if Kernel.const_defined?("ChefVaultCookbook")
     def self.data_bag_item(data_bag_name, data_bag_item, missing_ok=false)
 
-      raw_hash = Chef::DataBagItem.load(data_bag_name, data_bag_item)
-      encrypted = raw_hash.detect do |key, value|
-        if value.is_a?(Hash)
-          value.has_key?("encrypted_data")
-        end
-      end
-      if encrypted
-        if Chef::DataBag.load(data_bag_name).key? "#{data_bag_item}_keys"
-          chef_vault_item(data_bag_name, data_bag_item)
-        else
-          secret = Chef::EncryptedDataBagItem.load_secret
-          Chef::EncryptedDataBagItem.new(raw_hash, secret)
-        end
-      else
-        raw_hash
+      case ChefVault::Item.data_bag_item_type(data_bag_name, data_bag_item)
+      when :normal
+        Chef::DataBagItem.load(data_bag_name, data_bag_item)
+      when :encrypted
+        raw_hash = Chef::DataBagItem.load(data_bag_name, data_bag_item)
+        secret = Chef::EncryptedDataBagItem.load_secret
+        Chef::EncryptedDataBagItem.new(raw_hash, secret)
+      when :vault
+        chef_vault_item(data_bag_name, data_bag_item)
       end
 
     rescue Chef::Exceptions::ValidationFailed,
